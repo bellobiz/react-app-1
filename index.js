2162fb996d0a2be6d46ffdfdbbb01204c4d1c39c
Bello Osagie <osagienoah555@gmail.com>
2020-03-17 10:32:10 +0100

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Myapp from './Person/Person';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import "./index.css";
 
-const Person = (prop) => {
-  return (
-    <div className="person">
-      <App />
-      <h2>Name: {prop.name}</h2>
-      <p>Age: {prop.age}</p>
-      <Myapp />
-    </div>
-  );
-};
-
-var display = (
-  <div>
-    <Person name='Max' age='28' />
-    <Person name='Manu' age='29' />
-    <Person name='Bello' age='26' />
-  </div>
-);
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(display, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
